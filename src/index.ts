cd891e66eb869c2b1569d50e1f2e02fa93ff40f1
McMomo <dev.sleepapi@gmail.com>
2024-12-23 12:23:33 +0100

    feat: explicitly set return type of vimic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { afterEach, vi } from "vitest";
+import { afterEach, MockInstance, SpyInstance, vi } from "vitest";
 
-const activeSpies = new Set<ReturnType<typeof vi.spyOn>>();
+const activeSpies = new Set<SpyInstance<any[], any>>();
 
 afterEach(() => {
   for (const spy of activeSpies) {
@@ -14,7 +14,7 @@ export function vimic<T extends Record<string, any>, TKey extends keyof T>(
   module: T,
   functionName: TKey,
   implementation: T[TKey]
-) {
+): MockInstance<Parameters<T[TKey]>, ReturnType<T[TKey]>> {
   if (typeof module[functionName] !== "function") {
     throw new Error(
       `${String(functionName)} is not a function in the provided module.`
@@ -23,7 +23,10 @@ export function vimic<T extends Record<string, any>, TKey extends keyof T>(
 
   const spy = vi
     .spyOn(module, functionName as any)
-    .mockImplementation(implementation as T[TKey]);
+    .mockImplementation(implementation as T[TKey]) as unknown as MockInstance<
+    Parameters<T[TKey]>,
+    ReturnType<T[TKey]>
+  >;
   activeSpies.add(spy);
 
   return spy;
